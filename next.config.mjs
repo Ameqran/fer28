ea1941badf78c42142b2961d5b909a8dd98c8df0
Marouane Boukedir <marouane.boukedir@construo.ch>
2026-04-28 14:59:47 +0200

    Add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? '/fer28' : '',
+  assetPrefix: isGithubActions ? '/fer28/' : '',
 };
 
 export default nextConfig;
